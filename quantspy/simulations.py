c338e9ff1abe9c51390f49fd22c25cac9bfb1935
ericyerongjun <yerongjun03@gmail.com>
2026-02-21 15:09:05 +0800

    Add the classes for simulations first

---
+++ quantspy/simulations.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.stats import norm
+from typing import Union, Optional
+
+class GeometricBrownianMotion:
+
+class StochasticVarianceModel:
+    
+class MonteCarloBinaryCall:
+    
+class MonteCarloBinaryPut:
+    
+class MonteCarloCall:
+    
+class MonteCarloPut:
+    
+class MonteCarloAsianCall:
+    
+class MonteCarloAsianPut:
+    
+class MonteCarloBarrierCall:
+    
+class MonteCarloBarrierPut:
+    
+class MonteCarloExtendibleCall:
+    
+class MonteCarloExtendiblePut:
---
+++ quantspy/stochastics.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from scipy.stats import norm
+
+# Abstract class framework for a stochastic process
+class StochasticModel:
